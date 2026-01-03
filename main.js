a056458f8fc6d4506d9acfdce4cc6036e219367f
Thummala Hem Pradeep Royal <pradeeproyalthummala@Thummalas-MacBook-Pro.local>
2026-01-03 17:22:46 +0530

    Adds ddetails in template file

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-const ideaTemplate = `# Title Here
-
-## 📌 Basic Information
-- **Name**:
----
-
-## Problem Statement
-__write the problem statement here__
-
----
-
-## 💡 Idea Summary
-__share your thoughts__`;
-
 const displayThePath = (path) => {
   console.log(
     `Your idea is registered, to modify the details refer file://${Deno.cwd()}/README.md`,
@@ -22,20 +8,36 @@ const displayThePath = (path) => {
   console.log("Thanks for Sharing 🤍");
 };
 
-export const createIdeaFile = async (path) => {
-  await Deno.writeTextFile(path, ideaTemplate);
+const getTemplate = (name, idea) => {
+  return `# ${idea}
+### 📌 Basic Information
+- **Name**: ${name}
+- **GitHub repo**: nil (modify if exists).
+---
+### Problem Statement
+__write the problem statement here__
+---
+### 💡 Idea Summary
+_share your thoughts here_`;
+};
+
+export const createIdeaFile = async ({ path, name, idea }) => {
+  const template = getTemplate(name, idea);
+  await Deno.writeTextFile(path, template);
   displayThePath(path);
 };
 
-export const registerIdea = async ({ name, idea, path }) => {
+export const registerIdea = async (details) => {
+  const { name, idea, path } = details;
   const field = `| ${idea} | ${name} | [View Idea](./${path}) |\n`;
   await Deno.writeTextFile("./README.md", field, { append: true });
-  await createIdeaFile(path);
+  await createIdeaFile(details);
 };
 
 export const collectData = () => {
   const name = prompt("Enter your name > ");
-  const idea = prompt("Enter the title of the idea use Kebab-case > ") || 'idea';
+  const idea = prompt("Enter the title of the idea use Kebab-case > ") ||
+    "idea";
   const empId = prompt("enter the employee id > ");
   return { name, idea, empId, path: `ideas/${idea}-${empId}.md` };
 };
@@ -46,4 +48,3 @@ const main = () => {
 };
 
 main();
-
